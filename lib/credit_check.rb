1bcc3341ff13c678c8347c163c5b35b0c293f2e3
Katie Lonsdale <katie952@gmail.com>
2026-04-16 09:49:11 -0600

    add validate_account_number and helper methods

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,52 @@
 card_number = "5541808923795240"
+valid_message = "The number [card number] is valid!"
+invalid_message = "The number [card number] is invalid!"
 
 # Your Luhn Algorithm Here
+
+#double value of every other digit
+
+def double_digits(card_number)
+  double_numbers = []
+  card_number.split("").each_with_index do |num, i|
+    if i.even?
+      double_numbers << num.to_i * 2
+    else
+      double_numbers << num.to_i
+    end
+  end
+  double_numbers
+end
+
+# if any num in array > 9, sum digits
+
+def sum_digits(double_numbers)
+  double_numbers.map do |num|
+    num > 9 ? num.digits.sum : num
+  end
+end
+
+# take sum of all digits
+
+def sum_all_digits(summed_numbers)
+  summed_numbers.sum
+end
+
+# check if sum is divisible by ten
+
+def divisible_by_ten?(sum)
+  sum % 10 == 0 ? true : false
+end
+
+def validate_account_number(card_number)
+  double_numbers = double_digits(card_number)
+  summed_numbers = sum_digits(double_numbers)
+  sum = sum_all_digits(summed_numbers)
+  divisible_by_ten?(sum)
+end
 
 # Output
 ## If it is valid, print "The number [card number] is valid!"
 ## If it is invalid, print "The number [card number] is invalid!"
+
+p validate_account_number(card_number)
