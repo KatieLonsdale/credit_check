cc6076eefccd1663dd5dc2866761ddfb460b285b
Katie Lonsdale <katie952@gmail.com>
2026-04-16 10:21:07 -0600

    Add CreditCheck class and tests

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,33 +1,31 @@
-# valid
-card_number = "5541808923795240"
-# invalid
-# card_number = "5541801923795240"
+class CreditCheck
+  attr_reader :card_number, :limit
 
-def double_every_other_digit(card_number)
-  double_numbers = []
-  card_number.split("").each_with_index do |num, i|
-    i.even? ? double_numbers << num.to_i * 2 : double_numbers << num.to_i
+  def initialize(card_number, limit)
+    @card_number = card_number
+    @limit = limit
   end
-  double_numbers
-end
 
-def sum_digits_greater_than_nine(double_numbers)
-  double_numbers.map { |num| num > 9 ? num.digits.sum : num }
-end
+  def is_valid?
+    return false if @card_number.length < 16
+    double_numbers = double_every_other_digit(@card_number)
+    summed_numbers = sum_digits_greater_than_nine(double_numbers)
+    divisible_by_ten?(summed_numbers.sum)
+  end
 
-def divisible_by_ten?(sum)
-  sum % 10 == 0 ? true : false
+  def double_every_other_digit(card_number)
+    double_numbers = []
+    card_number.split("").each_with_index do |num, i|
+      i.even? ? double_numbers << num.to_i * 2 : double_numbers << num.to_i
+    end
+    double_numbers
+  end
+  
+  def sum_digits_greater_than_nine(double_numbers)
+    double_numbers.map { |num| num > 9 ? num.digits.sum : num }
+  end
+  
+  def divisible_by_ten?(sum)
+    sum % 10 == 0 ? true : false
+  end
 end
-
-def return_message(card_number, result)
-  result ? "The number #{card_number} is valid!" : "The number #{card_number} is invalid!"
-end
-
-def validate_account_number(card_number)
-  double_numbers = double_every_other_digit(card_number)
-  summed_numbers = sum_digits_greater_than_nine(double_numbers)
-  result = divisible_by_ten?(summed_numbers.sum)
-  return_message(card_number, result)
-end
-
-p validate_account_number(card_number)
---
+++ spec/credit_check_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+require_relative '../lib/credit_check.rb'
+
+RSpec.describe do 
+  describe 'initialization' do
+    it 'exists and has a card number and limit' do
+      card_number = "5541808923795240"
+      credit_check = CreditCheck.new(card_number, 15000)
+      expect(credit_check).to be_an_instance_of(CreditCheck)
+      expect(credit_check.card_number).to eq(card_number)
+      expect(credit_check.limit).to eq(15000)
+    end
+  end
+
+  describe 'instance methods' do
+    describe "#is_valid?" do
+      it 'returns boolean of if card_number is valid' do
+        valid_card_number = "5541808923795240"
+        credit_check = CreditCheck.new(valid_card_number, 15000)
+        expect(credit_check.is_valid?).to be true
+
+        invalid_card_number = "5541801923795240"
+        invalid_credit_check = CreditCheck.new(invalid_card_number, 15000)
+        expect(invalid_credit_check.is_valid?).to be false
+      end
+      it 'returns false if the card_number is incomplete' do
+        incomplete_card_number = "5541802379520"
+        incomplete_credit_check = CreditCheck.new(incomplete_card_number, 15000)
+        expect(incomplete_credit_check.is_valid?).to be false
+      end
+    end
+  end
+end
